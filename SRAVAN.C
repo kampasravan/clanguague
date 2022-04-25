43118a29521c5bc9a30da96ee8d4861f17d69217
kampasravan <104355199+kampasravan@users.noreply.github.com>
2022-04-25 15:26:46 +0530

    Add files via upload

---
+++ SRAVAN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char address[80];
+	clrscr();
+	printf("enter the address\n");
+	scanf("%[a-z]",address);
+	printf("%s",address);
+	getch();
+
+ }
